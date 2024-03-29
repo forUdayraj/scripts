1587fbc5b447c73d19091a5ddc147036e3c2fecd
#Shivam_Ingale <shivamingale3@gmail.com>
2024-03-29 10:46:19 +0530

    Update android14.sh

--- android14.sh
+++ android14.sh
@@ -1,6 +1,19 @@
+echo "---------------------------------------------------Cloning Device Tree-----------------------------------------------------"
 
 git clone https://github.com/PixelOS-Devices/device_motorola_tundra.git -b fourteen device/motorola/tundra 
+
+echo "---------------------------------------------------Cloning Vendor Tree-----------------------------------------------------"
+
 git clone https://gitlab.com/raghavt20/proprietary_vendor_motorola_tundra vendor/motorola/tundra 
+
+echo "---------------------------------------------------Cloning Kernel Tree-----------------------------------------------------"
+
 git clone https://github.com/raghavt20/kernel_sm8350.git -b thirteen kernel/motorola/tundra
+
+echo "---------------------------------------------------Cloning qcom------------------------------------------------------------"
+
 git clone https://github.com/LineageOS/android_system_qcom.git system/qcom 
+
+echo "---------------------------------------------------Cloning hardware--------------------------------------------------------"
+
 git clone https://github.com/LineageOS/android_hardware_motorola.git hardware/motorola
