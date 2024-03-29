81094264a38dea67b236fde7c70df14a4d6da8b1
#Shivam_Ingale <shivamingale3@gmail.com>
2024-03-29 10:44:17 +0530

    Create android14.sh

---
+++ android14.sh
@@ -0,0 +1,6 @@
+
+git clone https://github.com/PixelOS-Devices/device_motorola_tundra.git -b fourteen device/motorola/tundra 
+git clone https://gitlab.com/raghavt20/proprietary_vendor_motorola_tundra vendor/motorola/tundra 
+git clone https://github.com/raghavt20/kernel_sm8350.git -b thirteen kernel/motorola/tundra
+git clone https://github.com/LineageOS/android_system_qcom.git system/qcom 
+git clone https://github.com/LineageOS/android_hardware_motorola.git hardware/motorola
