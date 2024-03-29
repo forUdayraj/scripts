4fd85d21cf663e38c06cd51b444e2f5de3ee5b1b
#Shivam_Ingale <shivamingale3@gmail.com>
2024-03-29 10:42:06 +0530

    Create android13.sh

---
+++ android13.sh
@@ -0,0 +1,17 @@
+#echo "---------------------------------------------------Cloning Device Tree-----------------------------------------------------"
+
+#git clone https://github.com/raghavt20/android_device_motorola_tundra.git device/motorola/tundra
+
+echo "---------------------------------------------------Cloning Vendor Tree-----------------------------------------------------"
+
+git clone https://gitlab.com/shivamingale3/tundra_vendor.git vendor/motorola/tundra
+
+echo "---------------------------------------------------Cloning Kernel Tree-----------------------------------------------------"
+git clone https://github.com/raghavt20/kernel_sm8350.git -b thirteen kernel/motorola/tundra 
+
+echo "---------------------------------------------------Cloning qcom------------------------------------------------------------"
+
+git clone https://github.com/LineageOS/android_system_qcom.git system/qcom 
+
+echo "---------------------------------------------------Cloning hardware------------------------------------------------------------"
+git clone https://github.com/LineageOS/android_hardware_motorola.git hardware/motorola
